3436f9727622bdaa2c55ff0b84f46641beb6ff8f
delfige <delfinagerea@gmail.com>
2024-09-17 20:50:52 -0300

    reportes medicos 2

--- src/app/prueba/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Prueba() {
-    return (
-      <main className="flex items-center justify-center min-h-screen">
-        <h1 className="text-red-500 text-3xl">prueba</h1>
-      </main>
-    );
-  }
-  
