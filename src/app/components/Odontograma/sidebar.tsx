54301566f00a4fadd973441b4f86042c8ea2d346
Mati <matnahuel@ejemplo.com>
2024-11-11 15:55:40 -0300

    BarxSideBarxGrid

---
+++ src/app/components/Odontograma/sidebar.tsx
@@ -0,0 +1,94 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+
+
+const Sidebar: React.FC = () => {
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+
+  useEffect(() => {
+   
+    const userData = JSON.parse(localStorage.getItem('userData') || '{}');
+    
+
+    if (userData.roles && userData.roles.some((role: { name: string }) => role.name === "admin")) {
+      setIsAdmin(true);
+    }
+  }, []);
+  return (
+    <div className='navbar min-w-[14%] min-h-[100%] sm:min-w-[100px] lg:min-w-[150px] xl:min-w-[170px] 2xl:min-w-[170px]'>
+      <div>
+        <Link className='a-navbar flex items-center justify-center' href="/dashboard">
+          <Image
+            className='logo h-8 w-[100%] md:h-10 lg:h-14 xl:h-14 2xl:h-14'
+            src="/assets/logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+          />
+        </Link>
+      </div>
+      <div className='div-links lg:items-start'>
+        <Link className='a-navbar justify-center sm:flex-col md:flex-col lg:flex-row lg:ml-[10%]' href="/dashboard">
+          <Image
+            src="/assets/hoy.png"
+            alt="logohoy"
+            className='hoy-logo h-10 w-10'
+            width={100 }
+            height={100}
+          />
+          <span className='hidden sm:block text-sm md:text-sm lg:text-md lg:justify-center xl:text-lg 2xl:text-lg'>Home</span>
+        </Link>
+
+        <Link className='a-navbar justify-center sm:flex-col md:flex-col lg:flex-row lg:ml-[10%]' href="/pacientes">
+          <Image
+            src="/assets/people.png"
+            alt="logopacientes"
+            className='pacientes-logo h-10 w-10'
+            width={60}
+            height={60}
+          />
+          <span className='hidden sm:block text-sm md:text-sm lg:text-md lg:justify-center xl:text-lg 2xl:text-lg'>Pacientes</span>
+        </Link>
+
+        <Link className='a-navbar justify-center sm:flex-col md:flex-col lg:flex-row lg:ml-[10%]' href="/turnos">
+          <Image
+            src="/assets/calendar.png"
+            alt="logocalendar"
+            className='calendar-logo h-16 w-10'
+            width={64}
+            height={64}
+          />
+          <span className='hidden sm:block text-sm md:text-sm lg:text-md lg:justify-center xl:text-lg 2xl:text-lg'>Turnos</span>
+        </Link>
+
+        <Link className='a-navbar justify-center sm:flex-col md:flex-col lg:flex-row lg:ml-[10%]' href="/profesionales">
+          <Image
+            src="/assets/doctor.png"
+            alt="logocalendar"
+            className='calendar-logo h-10 w-10'
+            width={60}
+            height={60}
+          />
+          <span className='hidden sm:block text-sm md:text-sm lg:text-md lg:justify-center xl:text-lg 2xl:text-lg'>Profesionales</span>
+        </Link>
+        {isAdmin && (
+        <Link className='a-navbar justify-center sm:flex-col md:flex-col lg:flex-row lg:ml-[10%]' href="/administracion">
+          <Image
+            src="/assets/registro.png"
+            alt="registro"
+            className='calendar-logo h-10 w-10'
+            width={60}
+            height={60}
+          />
+          <span className='hidden sm:block text-sm md:text-sm lg:text-md lg:justify-center xl:text-lg 2xl:text-lg'>Administracion</span>
+        </Link>
+         )}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/app/odontograma/page.tsx
+++ src/app/odontograma/page.tsx
@@ -2,40 +2,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ToothGrid from '../components/Odontograma/ToothGrid';
-import Navbar from '../components/Odontograma/Navbar';
 import Notas from '../components/Odontograma/Notas';
-import UserForm from '../components/Odontograma/userform'; 
+import Sidebar from '../components/Odontograma/sidebar';
+import Bar from '../components/Odontograma/bar';
 import './odonto.css';
 
 const App = () => {
   const [registros, setRegistros] = useState<{ lado: string; color: string; fecha: string; accion: string }[]>([]);
   const [highlightedTooth, setHighlightedTooth] = useState<{ number: number; color: string; lado: string } | null>(null);
-  const [showUserForm, setShowUserForm] = useState(false);
   const [users, setUsers] = useState<{ id: number; username: string }[]>([]);
   const [selectedUser, setSelectedUser] = useState<number>(1); // Valor por defecto 1
-  const [userData, setUserData] = useState<{ user: number } | null>(null); // Cambiado a nuevo tipo
+  const [userData, setUserData] = useState<{ user: number } | null>(null);
 
-  // Función para cargar usuarios
-  const fetchUsers = async () => {
-    try {
-      const response = await axios.get('http://localhost:8000/api/users/');
-      setUsers(response.data);
-    } catch (error) {
-      console.error('Error al cargar los usuarios:', error);
-    }
-  };
-
-  const handleUserRegister = async (username: string, password: string, role: 'paciente' | 'profesional') => {
-    const userData = { username, password, role };
-
-    try {
-      const response = await axios.post('http://localhost:8000/api/register/', userData);
-      console.log('Usuario creado:', response.data);
-      setShowUserForm(false);
-      fetchUsers();
-    } catch (error) {
-      console.error('Error al crear el usuario:', error);
-    }
+  // Función para obtener el token desde las cookies
+  const getAuthToken = () => {
+    return (
+      document.cookie
+        .split('; ')
+        .find((row) => row.startsWith('access_token='))
+        ?.split('=')[1] || ''
+    );
   };
 
   const handleRegister = (registro: { lado: string; color: string; fecha: string; accion: string }) => {
@@ -46,12 +32,17 @@ const App = () => {
     setHighlightedTooth({ number: toothNumber, color, lado });
   };
 
-  const toggleUserForm = () => {
-    setShowUserForm(!showUserForm);
-  };
-
-  const handleFinalButtonClick = () => {
-    setShowUserForm(true);
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/users/', {
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`, // Agregar el encabezado de autorización
+        },
+      });
+      setUsers(response.data);
+    } catch (error) {
+      console.error('Error al cargar los usuarios:', error);
+    }
   };
 
   useEffect(() => {
@@ -63,79 +54,27 @@ const App = () => {
   }, [selectedUser]);
 
   return (
-    <div>
-      <div className="background">
-        <Navbar userData={userData} /> {/* Pasando userData a Navbar */}
-
-        {/* Desplegable de usuarios */}
-        <div style={{ marginLeft: '500px' }}>
-          <select 
-            onChange={(e) => {
-              const selectedId = Number(e.target.value);
-              setSelectedUser(selectedId ? selectedId : 1); // Si no se selecciona, por defecto será 1
-              setUserData(selectedId ? { user: selectedId } : { user: 1 }); // Actualiza userData
-            }} 
-            value={selectedUser}
-          >
-            <option value="" disabled>Seleccione un usuario</option>
-            {users.map(user => (
-              <option key={user.id} value={user.id}>{user.username}</option>
-            ))}
-          </select>
-        </div>
-
-        {/* Botón para mostrar el formulario de usuario */}
-        <button onClick={toggleUserForm} style={{ margin: '20px' }}>
-          Crear Usuario
-        </button>
-
-        {/* Formulario para crear usuario */}
-        {showUserForm && (
-          <UserForm role="paciente" onClose={toggleUserForm} onRegister={handleUserRegister} />
-        )}
-
-        {/* Div flotante sobre ToothGrid */}
-        <div style={{ position: 'absolute', top: '385px', left: '370px', right: '0px', backgroundColor: 'black', height: '2px', width: '1190px' }}></div> 
-        <div style={{ position: 'absolute', bottom: '300px', left: '965px', height: '480px', width: '2px', backgroundColor: 'black'}}></div>
-
-        <div style={{ display: 'flex', flex: 1, alignItems: 'flex-start', width: '100%' }}>
-          <div className="logo-y-div1" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <div className="logo-container">
-              <img src="/assets/logo.png" alt="Logo" className="logo" />
-            </div>
-            <div className="div1">
-              <div className="botones-container">
-                <button className="boton">Botón</button>
-                <button className="boton">Botón</button>
-                <button className="boton">Botón</button>
-              </div>
-            </div>
-          </div>
-
-          <div className='contenedor-odonto' style={{ flex: 1, backgroundColor: '#f8f9fa' }}>
-            <ToothGrid 
-              onRegister={handleRegister} 
-              onHighlightTooth={onHighlightTooth} 
-              highlightedTooth={highlightedTooth} 
+    <div className="w-full flex min-h-screen">
+      <Sidebar />
+      <div className="w-[100%] flex-grow flex flex-col bg-[#F1F1F1]">
+        <Bar />
+        <div className="flex flex-col sm:flex-row justify-center gap-4 p-4">
+          <div className="contenedor-odonto" style={{ flex: 1, backgroundColor: '#f8f9fa' }}>
+            <ToothGrid
+              onRegister={handleRegister}
+              onHighlightTooth={onHighlightTooth}
+              highlightedTooth={highlightedTooth}
               userId={selectedUser} // Pasar el userId al componente ToothGrid
             />
           </div>
         </div>
-
-        <div className='notas-container'>
-          <Notas 
-            registros={registros} 
-            onRegister={handleRegister} 
-            onHighlightTooth={onHighlightTooth} 
+        <div className="notas-container">
+          <Notas
+            registros={registros}
+            onRegister={handleRegister}
+            onHighlightTooth={onHighlightTooth}
             userId={selectedUser} // Pasar el userId al componente Notas
           />
-        </div>
-
-        {/* Botón al final del todo que llama al formulario de usuario */}
-        <div style={{ textAlign: 'center', margin: '20px' }}>
-          <button onClick={handleFinalButtonClick} style={{ padding: '10px 20px', backgroundColor: '#007bff', color: 'white', border: 'none', borderRadius: '5px' }}>
-            Crear Usuario
-          </button>
         </div>
       </div>
     </div>
