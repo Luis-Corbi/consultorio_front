9b28e48f4933f8091ec20326c7d13a9a11d208f0
Luis Corbi <88495472+Luis-Corbi@users.noreply.github.com>
2024-11-12 18:48:50 -0300

    Revert "Matimerge"

--- src/app/icons/down.jsx
+++ src/app/icons/down.jsx
@@ -1,14 +1,6 @@
 const iconDown = () => (
-<<<<<<< HEAD
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-2">
-      <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-    </svg>);
-    
-    export default iconDown;
-=======
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-2">
   <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
 </svg>);
 
 export default iconDown;
->>>>>>> 2f99b91aefb6c19e0a86fc8b69e14e74f0bc2627
